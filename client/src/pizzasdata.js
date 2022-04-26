36063a1fea9afd1476bf83cbdde0b7b9860bea33
Chubbs91 <chaskin1991@outlook.com>
2022-04-25 20:22:58 -0500

    fixing commit issues

---
+++ client/src/pizzasdata.js
@@ -0,0 +1,95 @@
+[
+  {
+    name: "Cheese",
+    size: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 11.99,
+        medium: 13.99,
+        large: 15.99,
+      },
+    ],
+    category: "nonveg",
+    image:
+      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2022%2F02%2F15%2Fclassic-cheese-pizza-FT-RECIPE0422.jpg&q=60",
+    description: "Our classic Cheese Pizza.",
+  },
+  {
+    name: "Pepperoni",
+    size: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 11.99,
+        medium: 13.99,
+        large: 15.99,
+      },
+    ],
+    category: "nonveg",
+    image:
+      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60",
+    description: "Zesty Pepperoni with a bit of kick.",
+  },
+  {
+    name: "Sausage",
+    size: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 11.99,
+        medium: 13.99,
+        large: 15.99,
+      },
+    ],
+    category: "nonveg",
+    image: "https://images.media-allrecipes.com/userphotos/8229081.jpg",
+    description: "Our Spicy Sausage Pizza.",
+  },
+  {
+    name: "Vegie",
+    size: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 11.99,
+        medium: 13.99,
+        large: 15.99,
+      },
+    ],
+    category: "veg",
+    image:
+      "https://theclevermeal.com/wp-content/uploads/2021/11/veggie-pizza_05-799x1024.jpg",
+    description: "Our Classic Vegetarian Pizza.",
+  },
+  {
+    name: "Ultimate Vegan",
+    size: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 11.99,
+        medium: 13.99,
+        large: 15.99,
+      },
+    ],
+    category: "veg",
+    image:
+      "https://theclevermeal.com/wp-content/uploads/2021/09/veggie-pizza-recipes_0008_pizza-with-peppers_1.jpg-799x1024.jpg",
+    description: "Our Favorite Vegie Pizza Recipe.",
+  },
+  {
+    name: "Bacon Cheeseburger",
+    size: ["small", "medium", "large"],
+    prices: [
+      {
+        small: 11.99,
+        medium: 13.99,
+        large: 15.99,
+      },
+    ],
+    category: "nonveg",
+    image:
+      "https://www.caseys.com/medias/sys_master/images/h90/h44/8796494594078/8143_base-400x400/8143-base-400x400.png",
+    description: "Our Twist on Bacon Cheeseburger Pizza.",
+  },
+];
+
+// Make change to file
+
+// Add Second Comment
