5ea769edee9168c502549d0cd0a202e1e277390c
Chubbs91 <chaskin1991@outlook.com>
2022-04-27 16:27:02 -0500

    pizza model created

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 
+const Pizza = require("./models/pizzaModel");
+
 const app = express();
 const db = require("./db.js");
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is Working");
+});
+
+app.get("/getpizzas", (req, res) => {
+  Pizza.find({}, (err, docs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(docs);
+    }
+  });
 });
 
 const port = process.env.PORT || 5000;
