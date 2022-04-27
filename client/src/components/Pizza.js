2d437814b4718fd755b8e1d511baa0baa9999448
Chubbs91 <chaskin1991@outlook.com>
2022-04-27 14:30:16 -0500

    installed react-bootstrap.  Modals created for pizzas and  descriptions

--- client/src/components/Pizza.js
+++ client/src/components/Pizza.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 export default function Pizza({ pizza }) {
   const [quantity, setQuantity] = useState(1);
   const [size, setSize] = useState("small");
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div
       style={{ margin: "70px" }}
       className="shadow-lg p-3 mb-5 bg-white rounded"
     >
-      <h1>{pizza.name}</h1>
-      <img
-        src={pizza.image}
-        className="img-fluid"
-        style={{ height: "200px", width: "200px" }}
-      />
+      <div onClick={handleShow}>
+        <h1>{pizza.name}</h1>
+        <img
+          src={pizza.image}
+          className="img-fluid"
+          style={{ height: "200px", width: "200px" }}
+        />
+      </div>
 
       <div className="flex-container">
         <div className="w-100 m-1">
@@ -58,6 +67,27 @@ export default function Pizza({ pizza }) {
           <button className="btn">ADD TO CART</button>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{pizza.name}</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <img
+            src={pizza.image}
+            className="img-fluid"
+            style={{ height: "400px" }}
+          />
+          <p>{pizza.description}</p>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <button className="btn" onClick={handleClose}>
+            CLOSE
+          </button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
