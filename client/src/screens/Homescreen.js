07ecca5c436495f501e72b66439e45b2fa06dd52
autumnrose <Arosebarrios95@gmail.com>
2022-04-27 22:15:53 -0500

    More pizza stuff

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,20 +1,40 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState , useEffect } from "react";
+import {useDispatch , useSelector} from 'react-redux';
+import { getAllPizzas } from '../actions/pizzaActions';
 import Pizza from "../components/Pizza";
-import pizzas from "../pizzasdata";
+
 
 export default function Homescreen() {
+
+  const dispatch = useDispatch()
+
+const pizzasstate = useSelector(state=>state.getAllPizzasReducer)
+
+const {pizzas , error , loading} = pizzasstate
+
+  useEffect(() => {
+    dispatch(getAllPizzas())
+
+  }, []);  
+   
   return (
     <div>
-      <div className="row">
-        {pizzas.map((pizza) => {
-          return (
-            <div className="col-md-4">
-              <div>
-                <Pizza pizza={pizza} />
-              </div>
-            </div>
-          );
-        })}
+      <div className="row justify-content-center">
+
+        {loading ? (<h1>loading</h1>) : error ? (<h1>Somthing went wrong</h1>) : (
+pizzas.map((pizza) => {
+  return (
+    <div className="col-md-3 m-3" key={pizza._id}>
+      <div>
+        <Pizza pizza={pizza} />
+      </div>
+    </div>
+  );
+})
+        )}
+
+        
       </div>
     </div>
   );
