abc991151ec32e3965c69ae1757eaf62e71bdcfa
Chubbs91 <chaskin1991@outlook.com>
2022-04-28 13:58:37 -0500

    minor cleanup

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,40 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React, { useState , useEffect } from "react";
-import {useDispatch , useSelector} from 'react-redux';
-import { getAllPizzas } from '../actions/pizzaActions';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPizzas } from "../actions/pizzaActions";
 import Pizza from "../components/Pizza";
 
-
 export default function Homescreen() {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
+  const pizzasstate = useSelector((state) => state.getAllPizzasReducer);
 
-const pizzasstate = useSelector(state=>state.getAllPizzasReducer)
-
-const {pizzas , error , loading} = pizzasstate
+  const { pizzas, error, loading } = pizzasstate;
 
   useEffect(() => {
-    dispatch(getAllPizzas())
+    dispatch(getAllPizzas());
+  }, []);
 
-  }, []);  
-   
   return (
     <div>
       <div className="row justify-content-center">
-
-        {loading ? (<h1>loading</h1>) : error ? (<h1>Somthing went wrong</h1>) : (
-pizzas.map((pizza) => {
-  return (
-    <div className="col-md-3 m-3" key={pizza._id}>
-      <div>
-        <Pizza pizza={pizza} />
-      </div>
-    </div>
-  );
-})
+        {loading ? (
+          <h1>loading</h1>
+        ) : error ? (
+          <h1>Somthing went wrong</h1>
+        ) : (
+          pizzas.map((pizza) => {
+            return (
+              <div className="col-md-3 m-3" key={pizza._id}>
+                <div>
+                  <Pizza pizza={pizza} />
+                </div>
+              </div>
+            );
+          })
         )}
-
-        
       </div>
     </div>
   );
