b240a35cece7f0156b6e4163e2248fcd3517cbcb
Chubbs91 <chaskin1991@outlook.com>
2022-04-28 16:49:02 -0500

    installed reduxjs/toolkit and fixed createStore in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,22 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from "redux";
 
-import {createStore , applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
+import { configureStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import {composeWithDevTools} from 'redux-devtools-extension'
-import { getAllPizzasReducer } from './reducers/pizzaReducers'
+import { composeWithDevTools } from "redux-devtools-extension";
+import { getAllPizzasReducer } from "./reducers/pizzaReducers";
 
 const finalReducer = combineReducers({
-    getAllPizzasReducer : getAllPizzasReducer
-})
-const initialState = {}
+  getAllPizzasReducer: getAllPizzasReducer,
+});
+const initialState = {};
 
-const composeEnhancers= composeWithDevTools({})
+const composeEnhancers = composeWithDevTools({});
 
-const store = createStore(finalReducer , initialState , composeEnhancers(applyMiddleware(thunk)))
+const store = configureStore(
+  finalReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
-export default store 
+export default store;
