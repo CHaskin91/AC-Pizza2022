beece47fb45ef1d37897d830193cebc6bdff5651
Chubbs91 <chaskin1991@outlook.com>
2022-04-28 23:00:16 -0500

    App.js updates. Cartscreen.js created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
 
@@ -8,7 +9,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Homescreen />
+      <BrowserRouter>
+        <Route path="/" exact component={Homescreen} />
+      </BrowserRouter>
     </div>
   );
 }
