91c09f4a1c37f4edc62f527d50e1487f5757a6f5
Chubbs91 <chaskin1991@outlook.com>
2022-04-29 14:18:39 -0500

    Cartscreen.js progress.

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
-
+import Cartscreen from "./screens/Cartscreen";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <BrowserRouter>
         <Route path="/" exact component={Homescreen} />
+        <Route path="/cart" exact component={Cartscreen} />
       </BrowserRouter>
     </div>
   );
