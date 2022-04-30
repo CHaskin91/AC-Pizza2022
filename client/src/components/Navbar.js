5f93bc3ab37d5c17f2c97a3ea6e632e46f515d27
Chubbs91 <chaskin1991@outlook.com>
2022-04-30 15:05:25 -0500

    additional cleanup on Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,9 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../actions/userActions";
 export default function Navbar() {
   const cartstate = useSelector((state) => state.cartReducer);
-  const userstate = useSelector(state=>state.loginUserReducer);
-  const {currentUser} = userstate;
-  const dispatch = useDispatch()
+  const userstate = useSelector((state) => state.loginUserReducer);
+  const { currentUser } = userstate;
+  const dispatch = useDispatch();
   return (
     <div>
       <nav className="navbar navbar-expand-lg shadow-lg p-3 mb-5 bg-white rounded">
@@ -25,28 +25,43 @@ export default function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
-          {currentUser ? (
-          <div className="dropdown mt-2">
-            <a style={{color: 'black'}} className="dropdown-toggle" type="button" id="dropdownMenuButtion" datatype="dropdown" aria-expanded="false">
-            {currentUser.name}
-            </a>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <a className="dropdown-item" href="#">Orders</a>
-              <a className="dropdown-item" href="#" onClick={()=>{dispatch(logoutUser())}}><li>Logout</li></a>
-          </div>
-        </div>  
-          ) : ( 
-          <li className="nav-item">
-            <a className="nav-link" href="/login">
-              login
-            </a>
-          </li>)}
-
-            <li className="nav-item">
-              <a className="nav-link" href="/login">
-                Login
-              </a>
-            </li>
+            {currentUser ? (
+              <div className="dropdown mt-2">
+                <a
+                  style={{ color: "black" }}
+                  className="dropdown-toggle"
+                  type="button"
+                  id="dropdownMenuButtion"
+                  datatype="dropdown"
+                  aria-expanded="false"
+                >
+                  {currentUser.name}
+                </a>
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton"
+                >
+                  <a className="dropdown-item" href="#">
+                    Orders
+                  </a>
+                  <a
+                    className="dropdown-item"
+                    href="#"
+                    onClick={() => {
+                      dispatch(logoutUser());
+                    }}
+                  >
+                    <li>Logout</li>
+                  </a>
+                </div>
+              </div>
+            ) : (
+              <li className="nav-item">
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              </li>
+            )}
             <li className="nav-item">
               <a className="nav-link" href="/cart">
                 Cart {cartstate.cartItems.length}
