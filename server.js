32e868d79409d0666d7203e1710ded8fa7d5fac1
Chubbs91 <chaskin1991@outlook.com>
2022-04-29 23:02:08 -0500

    stripe back-end started. orderRoute created

--- server.js
+++ server.js
@@ -6,15 +6,17 @@ const app = express();
 const db = require("./db.js");
 app.use(express.json());
 
-const pizzasRoute = require('./routes/pizzasRoute')
+const pizzasRoute = require("./routes/pizzasRoute");
 
-app.use('/api/pizzas/', pizzasRoute)
+const ordersRoute = require("./routes/ordersRoute");
+
+app.use("/api/pizzas/", pizzasRoute);
+
+app.use("/api/orders/", ordersRoute);
 
 app.get("/", (req, res) => {
   res.send("Server is Working");
 });
-
-
 
 const port = process.env.PORT || 5000;
 
